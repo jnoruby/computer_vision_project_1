0e26fcfb7421b5369fa74db48babe1ce34b31854
Michael Ruby <ruby@gen2.me>
2020-03-03 13:05:15 -0500

    implement return of zeroed out box filter

--- denoise.py
+++ denoise.py
@@ -11,11 +11,16 @@ cameraman = cv.imread('cameraman.png')
 multiplekeys = cv.imread('multiplekeys.png')
 
 # Show original images
-cv.imshow('original Lena',lena)
-cv.imshow('original cameraman', cameraman)
-cv.imshow('original multiple keys', multiplekeys)
-cv.waitKey(0)
+# cv.imshow('original Lena',lena)
+# cv.imshow('original cameraman', cameraman)
+# cv.imshow('original multiple keys', multiplekeys)
+# cv.waitKey(0)
+
+# Intialize zeroed out box filter of user-defined size
+box_filter_size = int(input("Set size of box filter: "))
+g = denoise.create_box_filter(box_filter_size)
+print(g)
 
 # Destroy all OpenCV windows
-cv.destroyAllWindows()
+# cv.destroyAllWindows()
 
--- image_denoising.py
+++ image_denoising.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
 def create_box_filter(n):
     # Enforce odd dimensions on filter, given even input by user
     if n % 2 == 0:
         n += 1
-    # Create box filter g of size (n * n)
+    # Create zeroed box filter g of size (n * n)
+    g = np.zeros((n, n), dtype=float)
     return g
 """
 def create_gaussian_filter(sigma):
