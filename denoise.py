a4694adb4a0a91ad157c62f02045a8351de9a38d
Michael Ruby <ruby@gen2.me>
2020-03-03 18:22:34 -0500

    put test images in list

--- denoise.py
+++ denoise.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import numpy as np
+# import numpy as np
 import cv2 as cv
 import image_denoising as denoise
 
-# Load image files 
+# Load image files and organise in list
 lena = cv.imread('lena.png')
 cameraman = cv.imread('cameraman.png')
 multiplekeys = cv.imread('multiplekeys.png')
+images = [('lena.png', lena),
+          ('cameraman.png', cameraman),
+          ('multiplekeys.png', multiplekeys)]
 
 # Show original images
-# cv.imshow('original Lena',lena)
-# cv.imshow('original cameraman', cameraman)
-# cv.imshow('original multiple keys', multiplekeys)
-# cv.waitKey(0)
+for image in images:
+    cv.imshow(image[0], image[1])
+cv.waitKey(0)
+
+# Add Gaussian noise with mean 0 and standard deviation 0.1 to images
+
 
 # Intialize zeroed out box filter of user-defined size
 box_filter_size = int(input("Set size of box filter: "))
