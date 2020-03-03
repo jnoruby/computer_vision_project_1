eb21c5cda2114d3f516229ce99398c5d22931d7d
Michael Ruby <ruby@gen2.me>
2020-03-02 20:51:05 -0500

    Load original images. test cv.imshow()

--- denoise.py
+++ denoise.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import image_denoising
+import numpy as np
+import cv2 as cv
+import image_denoising as denoise
+
+# Load image files 
+lena = cv.imread('lena.png')
+cameraman = cv.imread('cameraman.png')
+multiplekeys = cv.imread('multiplekeys.png')
+
+# Show original Lena image
+cv.imshow('original Lena',lena)
+cv.waitKey(0)
+
+# Destroy all OpenCV windows
+cv.destroyAllWindows()
 
