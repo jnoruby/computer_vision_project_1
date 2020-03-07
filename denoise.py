2e8ca51ac802bfcc4d9aa1a38342a1e9efe2eaf8
Michael Ruby <ruby@gen2.me>
2020-03-07 16:25:01 -0500

    update -2 days

--- denoise.py
+++ denoise.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# import numpy as np
+import numpy as np
 import cv2 as cv
+from skimage import color
+from skimage import io
 import image_denoising as denoise
 
-# Load image files and organise in list
-lena = cv.imread('lena.png')
-cameraman = cv.imread('cameraman.png')
-multiplekeys = cv.imread('multiplekeys.png')
-images = [('lena.png', lena),
-          ('cameraman.png', cameraman),
-          ('multiplekeys.png', multiplekeys)]
+# Box filter test
 
-# Show original images
-for image in images:
-    cv.imshow(image[0], image[1])
+# Load user-defined image file and convert to grayscale
+fd = input("Please enter path to image file: ")
+im = color.rgb2gray(cv.imread(fd))
+# Show original image
+cv.imshow(fd, im)
 cv.waitKey(0)
 
-# Add Gaussian noise with mean 0 and standard deviation 0.1 to images
+# Add Gaussian noise with mean 0 and standard deviations to images
+sigmas = [0.1, 0.4, 0.7]
+for sigma in sigmas:
+    im_noise = im + (sigma * np.random.randn(*im.shape))
+    # Show noisy images
+    cv.imshow("sigma=" + str(sigma) + " " + fd, im_noise)
+    cv.waitKey(0)
 
-
-# Intialize zeroed out box filter of user-defined size
+# Intialize box filter of user-defined size
 box_filter_size = int(input("Set size of box filter: "))
 g = denoise.create_box_filter(box_filter_size)
 print(g)
 
 # Destroy all OpenCV windows
-# cv.destroyAllWindows()
+cv.destroyAllWindows()
 
